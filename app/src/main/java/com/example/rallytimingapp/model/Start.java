ec529e78774571c9fcb66e227e7b1b41030dfd6e
skmann1800 <89962321+skmann1800@users.noreply.github.com>
2022-08-26 12:18:41 +1200

    "Created Start Object"

---
+++ app/src/main/java/com/example/rallytimingapp/model/Start.java
@@ -0,0 +1,50 @@
+package com.example.rallytimingapp.model;
+
+public class Start {
+
+    private int startID;
+    private int startOrder;
+    private int stage;
+    private int carNum;
+    private int stageID;
+
+    public int getStartID() {
+        return startID;
+    }
+
+    public void setStartID(int startID) {
+        this.startID = startID;
+    }
+
+    public int getStartOrder() {
+        return startOrder;
+    }
+
+    public void setStartOrder(int startOrder) {
+        this.startOrder = startOrder;
+    }
+
+    public int getStage() {
+        return stage;
+    }
+
+    public void setStage(int stage) {
+        this.stage = stage;
+    }
+
+    public int getCarNum() {
+        return carNum;
+    }
+
+    public void setCarNum(int carNum) {
+        this.carNum = carNum;
+    }
+
+    public int getStageID() {
+        return stageID;
+    }
+
+    public void setStageID(int stageID) {
+        this.stageID = stageID;
+    }
+}
